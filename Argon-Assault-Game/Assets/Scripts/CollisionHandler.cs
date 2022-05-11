4dcfea857400b8acf9db6b97d84959d2a090a732
myxsan <yksl3461@gmail.com>
2022-05-11 22:23:05 +0300

    Created CollisionHandler.cs

---
+++ Argon-Assault-Game/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other) {
+        Debug.Log(this.name + " - Collided with - " + other.gameObject.name);
+    }
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log($"{this.gameObject.name} - triggered with - {other.gameObject.name}");
+    }
+}
