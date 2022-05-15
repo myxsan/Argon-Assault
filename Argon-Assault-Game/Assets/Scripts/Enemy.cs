8476f2509cc23aaad13abbf56c2d5fd5dfab9078
myxsan <yksl3461@gmail.com>
2022-05-15 19:42:16 +0300

    Added and implemented explosion vfx for enemies

--- Argon-Assault-Game/Assets/Scripts/Enemy.cs
+++ Argon-Assault-Game/Assets/Scripts/Enemy.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
     private void OnParticleCollision(GameObject other) {
         Debug.Log($"{this.gameObject.name} : hit by {other.gameObject.name}");
+        GameObject vfx = Instantiate(deathVFX, this.transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(this.gameObject);
     }
 }
