9bf2e4e3b007075feaaa687b490df44229276d7d
myxsan <yksl3461@gmail.com>
2022-05-15 20:16:03 +0300

    Added a score system

--- Argon-Assault-Game/Assets/Scripts/Enemy.cs
+++ Argon-Assault-Game/Assets/Scripts/Enemy.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+
+    [Header("Score")]
+    ScoreBoard scoreBoard;
+    [SerializeField] int scorePerHit = 15;
+
+    private void Awake() {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
     private void OnParticleCollision(GameObject other) {
         Debug.Log($"{this.gameObject.name} : hit by {other.gameObject.name}");
         GameObject vfx = Instantiate(deathVFX, this.transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
+        scoreBoard.IncreaseScore(scorePerHit);
         Destroy(this.gameObject);
     }
 }
